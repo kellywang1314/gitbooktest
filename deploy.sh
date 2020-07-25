b284b54cd4f98b368e3407e40b41caffb3fab34f
王亚男 <wangyanan05@koolearn-inc.com>
2020-07-25 19:13:36 +0800

    fix:源代码提交

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,9 @@ set -e
 # 生成静态文件
 npm run build:docs
 # 进入生成的文件夹
-cd ./public
+cd ./_book
 git add -A
 git commit -m '更新文档'
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:kellywang1314/vuepresstest.git master:gh-pages
-rm -rf ./public
+rm -rf ./_book
